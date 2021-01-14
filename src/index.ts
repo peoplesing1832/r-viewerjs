6d0a13ef43046961c69fdc78bff9ed6fca77cf2c
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2021-01-14 15:26:51 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,53 @@
+import Viewer from 'viewerjs';
 import 'viewerjs/dist/viewer.min.css';
 
-export { default as Viewer } from './viewer';
-export { default as Viewers } from './viewers';
-export { default as view } from './view';
+type Image = Partial<Omit<HTMLImageElement, 'src'>> & {
+  src: string;
+};
+
+type Options = Viewer.Options & {
+  errImg?: string;
+}
+
+const createElement = (
+  images: Image[],
+  errImg: string,
+  viewer: Viewer
+) => {
+  const ul = document.createElement('ul');
+  const fragment = document.createDocumentFragment();
+  images.forEach((image, index) => {
+    const li = document.createElement('li');
+    const imgEle = document.createElement('img');
+    const entries = Object.entries(image);
+    entries.forEach(([key, value]) => imgEle.setAttribute(key, value as any));
+    if (errImg && viewer) {
+      imgEle.addEventListener('error', () => {
+        image.setAttribute('src', errImg);
+        viewer.update();
+        viewer.view(index);
+      });
+    }
+    fragment.appendChild(li.appendChild(imgEle));
+  });
+  ul.appendChild(fragment);
+};
+
+const createViewer = (
+  image: Image | Image[],
+  options?: Options,
+) => {
+  if (!Array.isArray(image)) {
+    image = [image]
+  }
+  let viewer
+  const { errImg = '', ...args } = options;
+
+  const ele = createElement(image, errImg, viewer);
+  viewer = new Viewer(ele, {
+    ...args,
+  });
+  return viewer;
+};
+
+export default createViewer;
